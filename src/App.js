707083aed1c7300bacd94bdd707f6f8e88377a30
Zelalem Mekonnen <zelliott33@gmail.com>
2023-09-25 15:57:02 -0500

    save as pdf completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 import ChooseDesign from "./components/chooseDesign/chooseDesign";
 import ChooseColor from "./components/chooseColor/chooseColor";
-import ChooseFinish from "./components/chooseFinish/chooseFinish"
+import ChooseFinish from "./components/chooseFinish/chooseFinish";
+import { Helmet } from "react-helmet";
+
+
+
 function App() {
   const [colorMap, setColorMap] = useState({});
 
--- src/components/chooseFinish/chooseFinish.js
+++ src/components/chooseFinish/chooseFinish.js
@@ -1,9 +1,42 @@
-import React from "react"
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import React, { useRef } from "react";
 
-const ChooseFinish = ({DesignComponent, colorMap}) => {
-    
-    console.log(colorMap);
-    return (<DesignComponent colorMap={colorMap}/>)
-}
+const saveSvgAsPdf = async (svgElement, name) => {
+  const canvas = await html2canvas(svgElement);
+  const imgData = canvas.toDataURL("image/png");
 
-export default ChooseFinish
+  const pdf = new jsPDF();
+
+  pdf.addImage(imgData, "PNG", 0, 0, canvas.width * 0.1, canvas.height * 0.1);
+
+  // Save the PDF
+  pdf.save(`${name}.pdf`);
+};
+
+const divStyle = {
+  width: "50vw",
+  height: "50vh",
+};
+
+const ChooseFinish = ({ DesignComponent, colorMap }) => {
+      const componentName =
+        DesignComponent.name ||
+        DesignComponent.displayName ||
+        "UnnamedComponent";
+
+  console.log(colorMap);
+  const svgRef = useRef(null);
+  return (
+    <div style={divStyle}>
+      <div ref={svgRef}>
+        <DesignComponent colorMap={colorMap} />
+      </div>
+      <button onClick={() => saveSvgAsPdf(svgRef.current, componentName)}>
+        Save as PDF
+      </button>
+    </div>
+  );
+};
+
+export default ChooseFinish;
