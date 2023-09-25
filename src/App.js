a54dcd639242638bba459865b9d02534b7606582
Zelalem Mekonnen <zelliott33@gmail.com>
2023-09-25 17:18:19 -0500

    cover

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header/header"
 import ChooseDesign from "./components/chooseDesign/chooseDesign";
 import ChooseColor from "./components/chooseColor/chooseColor";
 import ChooseFinish from "./components/chooseFinish/chooseFinish";
+import logo from "./components/header/indoteak.png"
 
 import "./App.css";
 
 
 function App() {
   const [colorMap, setColorMap] = useState({});
-
   const [step, setStep] = useState(1);
   const [selectedDesign, setSelectedDesign] = useState(null);
+
+
 
   //next step buttons designs out of time
   const nextStep = () => {
@@ -23,8 +25,38 @@ function App() {
     setStep((prevStep) => prevStep - 1);
   };
 
+      const [hideSplash, setHideSplash] = useState(false);
+      const [showContent, setShowContent] = useState(false);
+
+      const handleClick = () => {
+        setHideSplash(true);
+      };
+      
+
+      useEffect(() => {
+        if (hideSplash) {
+          setTimeout(() => {
+            setShowContent(true);
+          }, 2000); // 2-second delay to fade out the splash screen
+        }
+      }, [hideSplash]);
+
+    useEffect(() => {
+      if (hideSplash) {
+        setTimeout(() => {
+          setShowContent(true);
+        }, 1000);
+      }
+    }, [hideSplash]);
   return (
     <div>
+      <div
+        className={`splash-screen ${hideSplash ? "hide" : ""}`}
+        onClick={handleClick}
+      >
+        <img src={logo} alt="logo" />
+      </div>
+
       <Header />
       {step === 1 && (
         <ChooseDesign
