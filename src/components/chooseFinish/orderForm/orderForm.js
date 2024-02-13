f672dbd50a21d147371eeaa555e54ebedfc08515
Zelalem Mekonnen <zelliott33@gmail.com>
2024-02-12 21:53:12 -0600

    work in progress on the form style

--- src/components/chooseFinish/orderForm/orderForm.js
+++ src/components/chooseFinish/orderForm/orderForm.js
@@ -37,86 +37,103 @@ const OrderForm = () => {
     <div>
       <form onSubmit={handleSubmit} className="form-container">
         <h2>Project Information</h2>
-        <label>
-          Name*
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Email*
-          <input
-            type="email"
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Firm
-          <input
-            type="text"
-            name="firm"
-            value={form.firm}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Project
-          <input
-            type="text"
-            name="project"
-            value={form.project}
-            onChange={handleChange}
-          />
-        </label>
+
+        <div className="form-group">
+          <label>
+            Name*
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Email*
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Firm
+            <input
+              type="text"
+              name="firm"
+              value={form.firm}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Project
+            <input
+              type="text"
+              name="project"
+              value={form.project}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
         <h2>Shipping Information</h2>
-        <label>
-          Address*
-          <input
-            type="text"
-            name="address"
-            value={form.address}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          City*
-          <input
-            type="text"
-            name="city"
-            value={form.city}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          State/Province*
-          <input
-            type="text"
-            name="state"
-            value={form.state}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          ZIP/Postal Code*
-          <input
-            type="text"
-            name="zip"
-            value={form.zip}
-            onChange={handleChange}
-            required
-          />
-        </label>
+        <div className="form-group">
+          <label>
+            Address*
+            <input
+              type="text"
+              name="address"
+              value={form.address}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            City*
+            <input
+              type="text"
+              name="city"
+              value={form.city}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            State/Province*
+            <input
+              type="text"
+              name="state"
+              value={form.state}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            ZIP/Postal Code*
+            <input
+              type="text"
+              name="zip"
+              value={form.zip}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
       </form>
       <Button textContent={"Submit"} onClick={handleSubmit} />
     </div>
