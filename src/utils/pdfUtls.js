f2c7fff34fe73a01ef7ca7038ed4239395671659
Zelalem Mekonnen <zelliott33@gmail.com>
2024-03-01 19:07:56 -0600

    fix save to pdf button

--- src/utils/pdfUtls.js
+++ src/utils/pdfUtls.js
@@ -4,14 +4,13 @@ import jsPDF from "jspdf";
 const saveSvgAsPdf = async (svgElement, name) => {
   const canvas = await html2canvas(svgElement);
   const imgData = canvas.toDataURL("image/png");
-  imgData.save();
 
   const pdf = new jsPDF();
 
   pdf.addImage(imgData, "PNG", 0, 0, canvas.width * 0.1, canvas.height * 0.1);
 
   // Save the PDF
-  pdf.save(`${name}.pdf`);
+  pdf.save(`${name}-Style.pdf`);
 };
 
 export default saveSvgAsPdf;
