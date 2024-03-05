29ee60e41c9a76f3b74924176776ceb7cccdd83a
Zelalem Mekonnen <zelliott33@gmail.com>
2024-03-05 01:29:26 -0600

    pdf works

--- src/utils/pdfUtls.js
+++ src/utils/pdfUtls.js
@@ -4,16 +4,19 @@ import "svg2pdf.js";
 const saveSvgAsPdf = async (svgElement, name) => {
   //  A4 PDF page in jsPDF is 210 mm wide and 297 mm high
   const pdf = new jsPDF();
-  const pageHeight = pdf.internal.pageSize.getHeight(); // A4 size height in mm
-  const svgHeightPx = 580; // SVG height in pixels
-  const svgHeightMm = svgHeightPx * (25.4 / 96); // Convert pixels to mm
-  const yOffset = pageHeight - svgHeightMm; // Calculate Y offset
-
+  console.log(svgElement);
   svgElement = svgElement.querySelector("svg");
+  
 
-  // pdf.text("Your Header Text Here", 105, 20, { align: "center" }); // Add centered header at the top
-  await pdf.svg(svgElement, 10, yOffset, 180, svgHeightMm); // Adjust width and X offset as needed
-  pdf.save(`${name}.pdf`);
+  pdf.setFont("Syne");
+  console.log(pdf.getFontList());
+  pdf.setFontSize(24)
+  pdf.text(`${name} Order Sample`, 60, 20);
+
+  // pdf.save(`${name}.pdf`);
+  pdf
+    .svg(svgElement, { x: 10, y: 50, width:190, height:200 })
+    .then(() => pdf.save(`${name}.pdf`)); // Adjust width and X offset as needed
 };
 
 export default saveSvgAsPdf;
